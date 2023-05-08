ddb142a1d69449e4b60cf27af92fb70970b0fb85
Palash Mittal <pmittal00@gmail.com>
2023-05-09 02:34:57 +0530

    Ancestors, grandparents complete

--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -1,5 +1,7 @@
 #include "Person.h"
 
+#include <iostream>
+
 Person::Person(std::string name, std::string gender, std::string mother, std::string father, GenePool *genePool) : _name(name)
 {
     _genePool = genePool;
@@ -19,8 +21,13 @@ Person::Person(std::string name, std::string gender, std::string mother, std::st
 
     if (mother != "???")
         _mother = _genePool->find(mother);
+    else
+        _mother = nullptr;
+
     if (father != "???")
         _father = _genePool->find(father);
+    else
+        _father = nullptr;
 }
 
 // Person Member Functions
@@ -47,6 +54,26 @@ Person *Person::father()
 std::set<Person *> Person::ancestors(PMod pmod)
 {
     std::set<Person *> result;
+    if (pmod == PMod::MATERNAL || pmod == PMod::ANY)
+    {
+        if (_mother != nullptr)
+        {
+            result.insert(_mother);
+            std::set<Person *> res2 = _mother->ancestors(PMod::ANY);
+            if (!res2.empty())
+                result.insert(res2.begin(), res2.end());
+        }
+    }
+    if (pmod == PMod::PATERNAL || pmod == PMod::ANY)
+    {
+        if (_father != nullptr)
+        {
+            result.insert(_father);
+            std::set<Person *> res2 = _father->ancestors(PMod::ANY);
+            if (!res2.empty())
+                result.insert(res2.begin(), res2.end());
+        }
+    }
     return result;
 }
 std::set<Person *> Person::aunts(PMod pmod, SMod smod)
@@ -92,16 +119,60 @@ std::set<Person *> Person::granddaughters()
 std::set<Person *> Person::grandfathers(PMod pmod)
 {
     std::set<Person *> result;
+    if (pmod == PMod::MATERNAL || pmod == PMod::ANY)
+    {
+        if (_mother != nullptr && _mother->father() != nullptr)
+        {
+            result.insert(_mother->father());
+        }
+    }
+    if (pmod == PMod::PATERNAL || pmod == PMod::ANY)
+    {
+        if (_father != nullptr && _father->father() != nullptr)
+        {
+            result.insert(_father->father());
+        }
+    }
     return result;
 }
 std::set<Person *> Person::grandmothers(PMod pmod)
 {
     std::set<Person *> result;
+    if (pmod == PMod::MATERNAL || pmod == PMod::ANY)
+    {
+        if (_mother != nullptr && _mother->mother() != nullptr)
+        {
+            result.insert(_mother->mother());
+        }
+    }
+    if (pmod == PMod::PATERNAL || pmod == PMod::ANY)
+    {
+        if (_father != nullptr && _father->mother() != nullptr)
+        {
+            result.insert(_father->mother());
+        }
+    }
+
     return result;
 }
 std::set<Person *> Person::grandparents(PMod pmod)
 {
     std::set<Person *> result;
+    if (pmod == PMod::MATERNAL || pmod == PMod::ANY)
+    {
+        if (_mother != nullptr)
+        {
+            result.insert(_mother->parents(PMod::ANY).begin(), _mother->parents(PMod::ANY).end());
+        }
+    }
+    if (pmod == PMod::PATERNAL || pmod == PMod::ANY)
+    {
+        if (_father != nullptr)
+        {
+            result.insert(_father->parents(PMod::ANY).begin(), _father->parents(PMod::ANY).end());
+        }
+    }
+
     return result;
 }
 std::set<Person *> Person::grandsons()
@@ -122,6 +193,14 @@ std::set<Person *> Person::nieces(PMod pmod, SMod smod)
 std::set<Person *> Person::parents(PMod pmod)
 {
     std::set<Person *> result;
+    if (_father != nullptr)
+    {
+        result.insert(_father);
+    }
+    if (_mother != nullptr)
+    {
+        result.insert(_mother);
+    }
     return result;
 }
 std::set<Person *> Person::siblings(PMod pmod, SMod smod)
